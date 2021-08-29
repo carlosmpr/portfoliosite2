96875624c8974d3fd449a4105c4da9cdfa1e0504
carlosp <carlosmpr0508@gmail.com>
2021-08-29 11:19:34 -0400

    Added social buttons
    Clean up personal info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,42 +1,31 @@
+
 import React from "react";
 import Avatar from "./Avatar";
 import Card from "./Card/Card";
+import Social from "./Social";
 export default function About() {
+
+  const me = [{text: "Hi, I'm Carlos Polanco.Full stack developer across multiple platforms📱💻, cloud architecture, and designing/testing/maintenance of web applications." , avatar:true} , { text:
+    "🎓4+ years of IT experience🎓"
+  },
+{text:  "💪🤝 Strong collaboration and problem-solving skills, a focus on innovation and creativity for accomplishing robust projects. Passionate about learning new things and staying current with the latest in technology."},
+{text:"My Skills are in Python, React, TypeScript, NodeJS, Ruby, Rails, Redux, Flutter, Ionic, NextJS, React-Native, SQL, VueJS, GraphQL, NoSQL, Git, Bash Script, Electron, AWS, Docker, PostgreSQL, Active Record, Heroku, Figma, Firebase, PWA"}
+]
   return (
     <div className="w-screen h-screen flex flex-col items-center  font-mono space-y-6 ">
       <h1 className="font-bold text-4xl">About ME</h1>
-
       <div className="w-full overflow:hidden space-y-6">
-        <div className="w-full  justify-center flex items-center ">
-          <Card
-            text={
-              "Hi, I'm Carlos Polanco.Full stack developer across multiple platforms📱💻, cloud architecture, and designing/testing/maintenance of web applications."
-            }
-          />
-          <Avatar />
-        </div>
-        <div className="w-full   justify-center  flex items-center">
-          <Card
-            text={
-              "🎓4+ years of IT experience🎓"
-            }
-          />
-        </div>
-        <div className="w-full justify-center  flex items-center">
-          <Card
-            text={
-             "💪🤝 Strong collaboration and problem-solving skills, a focus on innovation and creativity for accomplishing robust projects. Passionate about learning new things and staying current with the latest in technology."
-            }
-          />
-        </div>
-        <div className="w-full justify-center  flex items-center">
-          <Card
-            text={
-             "My Skills are in Python, React, TypeScript, NodeJS, Ruby, Rails, Redux, Flutter, Ionic, NextJS, React-Native, SQL, VueJS, GraphQL, NoSQL, Git, Bash Script, Electron, AWS, Docker, PostgreSQL, Active Record, Heroku, Figma, Firebase, PWA"
-            }
-          />
-        </div>
+         {me.map(data => {
+           return (
+            <div className="w-full  justify-center flex items-center ">
+             <Card {...data}/>
+             {data.avatar ? <Avatar /> :null}
+             </div>
+           )
+         })}
+      
       </div>
+      <Social />
     </div>
   );
 }
---
+++ src/components/Social.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Social() {
+    return (
+        <div className="w-screen  flex justify-evenly items-center  font-mono ">
+            <a className="bg-indigo-700 bg-opacity-20   backdrop-filter backdrop-blur overflow-hidden  shadow-lg p-4 rounded-xl">Github</a>
+            <a className="bg-blue-700 bg-opacity-25   backdrop-filter backdrop-blur overflow-hidden  shadow-lg p-4 rounded-xl">Linkedin</a>
+            <a className="bg-black bg-opacity-25   backdrop-filter backdrop-blur overflow-hidden  shadow-lg p-4 rounded-xl">Mediun</a>
+            <a className="bg-red-700 bg-opacity-25   backdrop-filter backdrop-blur overflow-hidden  shadow-lg p-4 rounded-xl">Resume</a>
+            
+        </div>
+    )
+}
