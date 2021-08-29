64ac418de319a657b9e3d79032766de0e17e4608
carlosp <carlosmpr0508@gmail.com>
2021-08-29 11:01:23 -0400

    Added Other Projects Information

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import "./App.css";
 import About from "./components/About";
 import Hero from "./components/Hero";
 import Navabr from "./components/Navabr";
+import Others from "./components/Others";
 import PortFolio from "./components/PortFolio";
 function App() {
   return (
     <>
       <Navabr />
       <Hero />
-      <About />
+      
       <PortFolio />
+      <Others />
+      <About />
     </>
   );
 }
---
+++ src/components/Others.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import OthersContent from "./Others/OthersContent";
+export default function Others() {
+  const others = [
+    {
+      title: "JBOOK",
+      description:
+        "Jupyter style notebook for javascriptwith in-browser bundling capabilities build using: TypesScrit,ESBUILD.",
+      github: "",
+    },
+
+    {
+        title: "EMAILY",
+        description:
+          "Eamily is an app to collect customer feedback, sending email blast with SendGrid API and managing payments with the stripe API.",
+        github: "",
+      },
+
+
+      {
+        title: "STREAMER",
+        description:
+          "Node server for streamer using node rtsp-stream packages and express that allows to stream from obs to the website.",
+        github: "",
+      },
+  ];
+  return (
+    <div className="w-screen  flex flex-col items-center  font-mono space-y-6 ">
+      <h1 className="font-bold text-4xl">Other Projects</h1>
+      <div className="w-full flex justify-evenly items-center flex-wrap">
+        {others.map((data) => (
+          <OthersContent {...data} />
+        ))}
+      </div>
+    </div>
+  );
+}
